7399db3a2a2d4d21f955c0d96f0a5136ca677128
James Wright <james@wrightomated.com>
2020-09-24 07:16:20 +1000

    switched to typescript

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ const io = require("socket.io");
 const server = io.listen(3000);
 
 server.on("connection", function (socket) {
-  console.log("user connected");
+  console.log("user copmnnected");
   socket.emit("welcome", "GOOO DUMPLING!");
 });
